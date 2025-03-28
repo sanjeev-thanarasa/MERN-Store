564eb97226e37189b6faadc307baf7671c8afd34
Sanjeev Thanarasa <59352357+sanjeev-thanarasa@users.noreply.github.com>
2025-03-28 22:36:22 +0530

    Cleaned and Structured:db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,13 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
-    process.exit(1);
+    process.exit(1); // Exit process with failure
   }
 };
