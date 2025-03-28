88af930403f4a3532aaa74c9101267af87e4e68f
Sanjeev Thanarasa <59352357+sanjeev-thanarasa@users.noreply.github.com>
2025-03-29 00:55:41 +0530

    Updated: DEPLOYMENT to PRODUCTION

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import express from "express"; // Express framework for building APIs
 import dotenv from "dotenv"; // For loading environment variables
 import { connectDB } from "./config/db.js"; // MongoDB connection setup
 import productRoutes from "./routes/product.route.js"; // Routes for product-related API endpoints
+import path from "path";
+import cors from "cors";
 
 // Load environment variables from .env file
 dotenv.config();
@@ -10,12 +12,28 @@ dotenv.config();
 // Initialize Express application
 const app = express();
 const PORT = process.env.PORT || 5001; // Use port from .env or fallback to 5001
+const __dirname = path.resolve();
 
 // Middleware to parse incoming JSON requests
 app.use(express.json());
 
 // Define API routes
 app.use("/api/products", productRoutes); // All product-related routes start with /api/products
+
+// Allow requests from the frontend
+app.use(cors({ origin: "http://localhost:5002" })); // Replace with your frontend URL
+
+if (!process.env.MONGO_URI) {
+  console.error("Error: MONGO_URI is not defined in the .env file");
+  process.exit(1); // Exit the process with failure
+}
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 // Error handler middleware to handle errors globally
 app.use((err, req, res, next) => {
