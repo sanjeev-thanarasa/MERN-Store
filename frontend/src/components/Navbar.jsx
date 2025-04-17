ef9aae761a2bc6d5eceab4500cf4397da1295652
Sanjeev Thanarasa <59352357+sanjeev-thanarasa@users.noreply.github.com>
2025-04-17 12:38:57 +0530

    Update Check for render.com

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -30,7 +30,7 @@ const Navbar = () => {
           bgGradient={"linear(to-r, cyan.400, blue.500)"}
           bgClip={"text"}
         >
-          <Link to={"/"}>Product Store 🛒</Link>
+          <Link to={"/"}>Product Store 🛒🛒🛒</Link>
         </Text>
         <HStack spacing={2} alignItems={"center"}>
           <Link to={"/create"}>
